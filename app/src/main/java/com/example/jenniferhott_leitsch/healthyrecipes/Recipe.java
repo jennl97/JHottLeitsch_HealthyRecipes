03d7b7850c79f8279ef5f04755af07bbb9f7b9d5
jennl97 <jennhott97@gmail.com>
2018-01-27 22:12:55 -0600

    Initial commit

---
+++ app/src/main/java/com/example/jenniferhott_leitsch/healthyrecipes/Recipe.java
@@ -0,0 +1,23 @@
+package com.example.jenniferhott_leitsch.healthyrecipes;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Recipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        };
+    }
+
+
